a1274abff8b071e2d6e9c0dedcb2606fc7b18297
Matti Aho <ookooko@gmail.com>
2021-03-05 04:26:41 +0200

    periocheck - should not be working

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  res.send('ok')
+  // res.send('ok'); // testing per health check
+  res.status(400).end();
 })
 
 app.get('/version', (req, res) => {
