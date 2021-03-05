56d42fe02c55e141d2a3db6d125ea6d95c88896a
Matti Aho <ookooko@gmail.com>
2021-03-05 07:38:01 +0200

    health check which should be OK, in every 2 hours

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  res.send('ok') // comment while health check
-  // res.status(400).end() // uncomment while health check
+  res.send('ok') //comment while health check
+  // res.status(400).end() //uncomment while health check
 })
 
 app.get('/version', (req, res) => {
